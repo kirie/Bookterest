66b0f8b2b4d788932387beb2786bb83583eefec2
Eirik <eiriklin@yahoo.com>
2016-10-24 15:28:44 -0700

    Add - authentication and package.json

---
+++ src/reducers/board_reducer.js
@@ -0,0 +1,18 @@
+import {FETCH_BOARD, ADD_PIN, DELETE_PIN} from '../actions/types';
+import update from 'react-addons-update';
+
+const INITIAL_STATE = {
+  board: []
+}
+
+export default function (state = INITIAL_STATE, action){
+  switch(action.type) {
+    case FETCH_BOARD:
+      return {...state, board: action.payload};
+    case ADD_PIN:
+      return update(state, {board: {$push: [action.payload]}});
+    case DELETE_PIN:
+      return {board: state.board.filter(val => val.id !== action.payload)};
+  }
+  return state;
+}
